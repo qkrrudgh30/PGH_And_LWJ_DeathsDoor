f92e7af130424dc6d465a1f065e5a5348d15f735
qkrrudgh30@naver.com <qkrrudgh30@gmail.com>
2022-11-16 21:13:06 +0900

    박경호 : Fade 셰이더 추가

--- ContentsResources/Shader/Fade.hlsl
+++ ContentsResources/Shader/Fade.hlsl
@@ -15,52 +15,52 @@ struct Output
     float4 Tex : TEXCOORD;
 };
 
-cbuffer AtlasData : register(b1)
+cbuffer FadeInfo : register(b1)
 {
-	float2 TextureFramePos;
-	float2 TextureFrameSize;
-	float4 PivotPos;
+    float2  mf2FrameDataPos;
+    float2  mf2FrameDataSize;
+    float4  mf4PivotPos;
+    float   mfFrom;
+    float   mfTo;
+    int     mbWrapping;
+    int     mbLoop;
+    int     mbIsUnityTexture;
 }
 
 Output Fade_VS(Input _Input)
 {
 	Output NewOutput = (Output)0;
 
-	_Input.Pos += PivotPos;
+    _Input.Pos += mf4PivotPos;
 	NewOutput.Pos = mul(_Input.Pos, WorldViewProjection);
+    
 	NewOutput.PosLocal = _Input.Pos;
 
-	NewOutput.Tex.x = (_Input.Tex.x * TextureFrameSize.x) + TextureFramePos.x;
-	NewOutput.Tex.y = (_Input.Tex.y * TextureFrameSize.y) + TextureFramePos.y;
+    NewOutput.Tex.x = (_Input.Tex.x * mf2FrameDataSize.x) + mf2FrameDataPos.x;
+    NewOutput.Tex.y = (_Input.Tex.y * mf2FrameDataSize.y) + mf2FrameDataPos.y;
 
 	return NewOutput;
-}
-
-cbuffer PixelData : register(b0)
-{
-    float4 MulColor;
-    float4 PlusColor;
-    float4 Slice;
 }
 
 Texture2D Tex : register(t0);
 
 SamplerState LINEARWRAP : register(s0);
 
-float4 Fade_PS(Output _Input)
+float4 Fade_PS(Output _Input) : SV_Target0
 {
-    if (_Input.Tex.x < Slice.x)
-    {
-        clip(-1);
-    }
-	
-    if (_Input.Tex.y < Slice.y)
+    if (_Input.Tex.x < 0.f || _Input.Tex.y < 0.f)
     {
         clip(-1);
     }
     
+    float4 Result = Tex.Sample(LINEARWRAP, _Input.Tex.xy);
     
-
+    if (1 <= Result.a)
+    {
+        Result.a = 1.0f;
+    }
+    
+    return Result;
 }
 
 
