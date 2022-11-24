aabf3d4b4dd18c59b898182ab177e900e6680c1c
qkrrudgh30@naver.com <qkrrudgh30@gmail.com>
2022-11-24 16:38:41 +0900

    박경호 : 코드 머지

---
+++ GameEngineContents/ContentsBlur.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <GameEngineCore/GameEngineRenderTarget.h>
+
+struct BlurInfo
+{
+	float4			mf4FrameData;
+	float4			mf4PivotPos;
+	unsigned int	muAppliedType;
+	unsigned int	muAppliedCount;
+	float			mfRadius;
+	int				miIsUnityTexture;
+};
+
+// Ό³Έν :
+class ContentsBlur : public GameEnginePostEffect
+{
+public:
+	// constrcuter destructer
+	ContentsBlur();
+	~ContentsBlur();
+
+	// delete Function
+	ContentsBlur(const ContentsBlur& _Other) = delete;
+	ContentsBlur(ContentsBlur&& _Other) noexcept = delete;
+	ContentsBlur& operator=(const ContentsBlur& _Other) = delete;
+	ContentsBlur& operator=(ContentsBlur&& _Other) noexcept = delete;
+
+	virtual void EffectInit();
+	virtual void Effect(std::shared_ptr<GameEngineRenderTarget> _sptrRenderTarget);
+	
+	void SetBlurInfo(unsigned int _uBlurType, unsigned int _uAppliedCount = 1u, float _fRadius = 0.f, int _iIsUnityTexture = 0);
+
+protected:
+
+private:
+	void InitializeBlurInfo(void);
+
+public:
+
+protected:
+
+private:
+	std::shared_ptr<GameEngineRenderTarget>	msptrRenderTargetForBlur;
+	GameEngineRenderUnit					mAppliedRenderUnit;
+	BlurInfo								mBlurInfo;
+
+};
+
