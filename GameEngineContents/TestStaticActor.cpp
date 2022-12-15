25423f312ad933a4cf15eb86db9f8acf6b0ee819
qkrrudgh30@naver.com <qkrrudgh30@gmail.com>
2022-12-15 18:52:49 +0900

    박경호 : 엔진 파트 테스트

--- GameEngineContents/TestStaticActor.cpp
+++ GameEngineContents/TestStaticActor.cpp
@@ -16,20 +16,20 @@ TestStaticActor::~TestStaticActor()
 
 void TestStaticActor::Start()
 {
-	/*std::shared_ptr<BillboardRenderer> sptrTestPicture = CreateComponent<BillboardRenderer>();
-	sptrTestPicture->SetTexture("TestPicture.png");
-	sptrTestPicture->GetTransform().SetLocalScale({ 500, 500, 1 });
-	sptrTestPicture->GetTransform().SetLocalRotation({ 45.f,0.f,0.f });
-	sptrTestPicture->GetTransform().SetLocalPosition({ -1500.f,1500.f, -250.f });
-	sptrTestPicture->ChangeCamera(CAMERAORDER::MAINCAMERA);
-	sptrTestPicture->SetBillboardInfo(1u);*/
+	std::shared_ptr<BillboardRenderer> sptrTestPicture1 = CreateComponent<BillboardRenderer>();
+	sptrTestPicture1->SetTexture("TestPicture.png");
+	sptrTestPicture1->GetTransform().SetLocalScale({ 500, 500, 1 });
+	sptrTestPicture1->GetTransform().SetLocalRotation({ 45.f,0.f,0.f });
+	sptrTestPicture1->GetTransform().SetLocalPosition({ -1500.f,1500.f, -250.f });
+	sptrTestPicture1->ChangeCamera(CAMERAORDER::MAINCAMERA);
+	sptrTestPicture1->SetBillboardInfo(1u);
 
-	std::shared_ptr<WaveRenderer> sptrTestPicture = CreateComponent<WaveRenderer>();
-	sptrTestPicture->SetTexture("TestPicture.png");
-	sptrTestPicture->GetTransform().SetLocalScale({ 500, 500, 1 });
-	sptrTestPicture->GetTransform().SetLocalRotation({ 45.f,0.f,0.f });
-	sptrTestPicture->GetTransform().SetLocalPosition({ -1500.f,1500.f, -250.f });
-	sptrTestPicture->ChangeCamera(CAMERAORDER::MAINCAMERA);
+	std::shared_ptr<WaveRenderer> sptrTestPicture2 = CreateComponent<WaveRenderer>();
+	sptrTestPicture2->SetTexture("TestPicture.png");
+	sptrTestPicture2->GetTransform().SetLocalScale({ 500, 500, 1 });
+	sptrTestPicture2->GetTransform().SetLocalRotation({ 45.f,0.f,0.f });
+	sptrTestPicture2->GetTransform().SetLocalPosition({ 1500.f,1500.f, -250.f });
+	sptrTestPicture2->ChangeCamera(CAMERAORDER::MAINCAMERA);
 
 	/*std::shared_ptr<FadeRenderer> sptrTestPicture = CreateComponent<FadeRenderer>();
 	sptrTestPicture->SetTexture("TestPicture.png");
@@ -39,10 +39,10 @@ void TestStaticActor::Start()
 	sptrTestPicture->ChangeCamera(CAMERAORDER::MAINCAMERA);
 	sptrTestPicture->SetFadeInfo(-1, 1.f, True);*/
 
-	Collision = CreateComponent<GameEngineCollision>();
-	Collision->GetTransform().SetWorldScale(float4{200.f, 100.f, 100.f});
-	Collision->SetParent(sptrTestPicture);
-	Collision->GetTransform().SetLocalPosition(float4{0.f, 0.f, 0.f});
+	// Collision = CreateComponent<GameEngineCollision>();
+	// Collision->GetTransform().SetWorldScale(float4{200.f, 100.f, 100.f});
+	// Collision->SetParent(sptrTestPicture);
+	// Collision->GetTransform().SetLocalPosition(float4{0.f, 0.f, 0.f});
 
 }
 
